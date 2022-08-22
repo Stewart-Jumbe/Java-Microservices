d46157e29a0d88f0a302e0278e842d65b4e17868
Stewart <stewartjumbe@gmail.com>
2022-08-22 22:27:23 +0100

    Implemented Get and Post Request methods

--- src/main/java/com/stewartjumbe/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/stewartjumbe/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,6 +1,12 @@
 package com.stewartjumbe.rest.webservices.restfulwebservices.user;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,6 +20,7 @@ public class UserResource {
 	
 	//GET /users
 	//retrieveAllUsers
+	@GetMapping("/users")
 	public List<User> retrieveAllUsers(){
 		
 		return service.findAll();
@@ -22,5 +29,18 @@ public class UserResource {
 	
 	//Get /users/{id}
 	//retrieveUser
+	@GetMapping("/users/{id}")
+	public User retrieveUSer(@PathVariable int id) {
+		return service.findOne(id);
+	}
+
+	//input - details of user
+	//output - Created & Return the created URI
+	@PostMapping("/users")
+	public void createUser(@RequestBody User user) {
+		//using @ReqestBody puts whatever is entered into the requestbody (via the url) as the input of the createUser method
+		User savedUser= service.save(user);
+		
+	}
 
 }
