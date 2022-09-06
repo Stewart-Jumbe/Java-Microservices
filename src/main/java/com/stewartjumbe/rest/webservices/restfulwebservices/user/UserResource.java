17cbb2e71b7a01b73fe38843ad7ef8cb45822fae
Stewart <stewartjumbe@gmail.com>
2022-09-06 16:59:31 +0100

    changed return type of PostMapping to return a created Response Entity

--- src/main/java/com/stewartjumbe/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/stewartjumbe/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,13 +1,16 @@
 package com.stewartjumbe.rest.webservices.restfulwebservices.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -37,9 +40,20 @@ public class UserResource {
 	//input - details of user
 	//output - Created & Return the created URI
 	@PostMapping("/users")
-	public void createUser(@RequestBody User user) {
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
 		//using @ReqestBody puts whatever is entered into the requestbody (via the url) as the input of the createUser method
 		User savedUser= service.save(user);
+		
+		//returning "Created" response
+		
+		//Getting the newly created user ID
+		// users/{id} --> users/{savedUser.getId()
+		URI location =ServletUriComponentsBuilder
+		.fromCurrentRequest()
+		.path("/{id}")
+		.buildAndExpand(savedUser.getId()).toUri();
+		
+		return ResponseEntity.created(location).build();
 		
 	}
 
