e4f4aa913ef59346ff6e3afd641d4f288ba2084f
Stewart <stewartjumbe@gmail.com>
2022-10-11 16:03:01 +0100

    connected api to mysql database hosted in docker, updated application properties, docker notes

--- src/main/java/com/stewartjumbe/rest/webservices/restfulwebservices/filtering/UserDetails.java
+++ src/main/java/com/stewartjumbe/rest/webservices/restfulwebservices/filtering/UserDetails.java
@@ -1,55 +1,55 @@
-package com.stewartjumbe.rest.webservices.restfulwebservices.filtering;
-
-import com.fasterxml.jackson.annotation.JsonFilter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonFilter("Dynamicfilter") // needed for dynamic filtering, filtering out password from response
-public class UserDetails {
-	
-	private String name;
-	private String email;
-	private String password;
-	
-	public UserDetails(String name, String email, String password) {
-		super();
-		this.name = name;
-		this.email = email;
-		this.password = password;
-		
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	@Override
-	public String toString() {
-		return "UserDetails [name=" + name + ", email=" + email + ", password=" + password + "]";
-	}
-	
-	
-
-	
-
-}
+//package com.stewartjumbe.rest.webservices.restfulwebservices.filtering;
+//
+//import com.fasterxml.jackson.annotation.JsonFilter;
+//import com.fasterxml.jackson.annotation.JsonIgnore;
+//import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+//
+//@JsonFilter("Dynamicfilter") // needed for dynamic filtering, filtering out password from response
+//public class UserDetails {
+//	
+//	private String name;
+//	private String email;
+//	private String password;
+//	
+//	public UserDetails(String name, String email, String password) {
+//		super();
+//		this.name = name;
+//		this.email = email;
+//		this.password = password;
+//		
+//	}
+//
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//
+//	public String getEmail() {
+//		return email;
+//	}
+//
+//	public void setEmail(String email) {
+//		this.email = email;
+//	}
+//
+//	public String getPassword() {
+//		return password;
+//	}
+//
+//	public void setPassword(String password) {
+//		this.password = password;
+//	}
+//
+//	@Override
+//	public String toString() {
+//		return "UserDetails [name=" + name + ", email=" + email + ", password=" + password + "]";
+//	}
+//	
+//	
+//
+//	
+//
+//}
--- src/main/java/com/stewartjumbe/rest/webservices/restfulwebservices/filtering/UserInfo.java
+++ src/main/java/com/stewartjumbe/rest/webservices/restfulwebservices/filtering/UserInfo.java
@@ -1,36 +1,36 @@
-package com.stewartjumbe.rest.webservices.restfulwebservices.filtering;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-public class UserInfo {
-	
-	String name ;
-	int age;
-	@JsonIgnore//password info will not be provided when user retrieves info from bean
-	String password;
-
-	public UserInfo(String name, int age, String password) {
-		this.name = name;
-		this.age = age;
-		this.password = password;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public int getAge() {
-		return age;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	@Override
-	public String toString() {
-		return "UserInfo [name=" + name + ", age=" + age + ", password=" + password + "]";
-	}
-	
-	
-}
+//package com.stewartjumbe.rest.webservices.restfulwebservices.filtering;
+//
+//import com.fasterxml.jackson.annotation.JsonIgnore;
+//
+//public class UserInfo {
+//	
+//	String name ;
+//	int age;
+//	@JsonIgnore//password info will not be provided when user retrieves info from bean
+//	String password;
+//
+//	public UserInfo(String name, int age, String password) {
+//		this.name = name;
+//		this.age = age;
+//		this.password = password;
+//	}
+//
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public int getAge() {
+//		return age;
+//	}
+//
+//	public String getPassword() {
+//		return password;
+//	}
+//
+//	@Override
+//	public String toString() {
+//		return "UserInfo [name=" + name + ", age=" + age + ", password=" + password + "]";
+//	}
+//	
+//	
+//}
